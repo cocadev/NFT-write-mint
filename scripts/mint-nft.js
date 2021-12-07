6a3280ce138842991f20c051baf7bbc7c8333364
cocadev <40970351+cocadev@users.noreply.github.com>
2021-12-07 12:40:45 +0800

    add nft meta data

--- scripts/mint-nft.js
+++ scripts/mint-nft.js
@@ -4,3 +4,4 @@ const { createAlchemyWeb3 } = require("@alch/alchemy-web3")
 const web3 = createAlchemyWeb3(API_URL)
 
 const contract = require("../artifacts/contracts/MyNFT.sol/MyNFT.json")
+console.log(JSON.stringify(contract.abi))
